7d8dd9689223b7d4496765a18f0419d2e34232d8
NGC602 <84300098+NGC602@users.noreply.github.com>
2021-07-11 18:22:13 -0700

    Add files via upload
    Re-made Yuzu download script and uploaded working RetroPie implimentation

---
+++ autoyuzu.sh
@@ -0,0 +1,26 @@
+#A script to download latest Yuzu AppImage for Linux and place it in a folder for later use with RetroPie
+
+#!/bin/bash
+
+# Curl script to reach out to GitHub API, look for download url relating to .AppImage, cut, tr & wget it
+curl -v -s https://api.github.com/repos/yuzu-emu/yuzu-mainline/releases/latest \
+| grep "browser_download_url.*AppImage" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi -
+
+# Make directory for recently CURL'ed file and move everything to it
+mkdir /home/console/Desktop/yuzu
+mv yuzu* /home/console/Desktop/yuzu
+
+# Make a safe place, move yuzu to it and then rename it to something digestible
+mv /home/console/Desktop/yuzu/yuzu-*.AppImage /opt/retropie/emulators/yuzu/bin/yuzu.AppImage
+
+# Get rid of the leftover stuff
+cd /home/console/Desktop
+rm -r yuzu
+
+#Allow execution of Yuzu.AppImage
+#chmod +x ~/yuzu/yuzu.AppImage
+
+exit 0
---
+++ yuzu.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+#
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="yuzu"
+rp_module_desc="Switch Emulator Yuzu"
+rp_module_help="ROM Extensions: .xci .nsi\n\nCopy your Switch games to $romdir/switch\n\nDon't forget to run system firmware update first!"
+rp_module_licence="GPL2 https://github.com/yuzu-emu/yuzu/blob/master/license.txt"
+rp_module_section="exp"
+rp_module_flags="!arm"
+
+function install_bin_yuzu() {
+    mkdir -p "$md_inst/bin"
+    sh /home/console/scripts/autoyuzu.sh -O "$md_inst/bin/yuzu.AppImage"
+    chmod +x "$md_inst/bin/yuzu.AppImage"
+}
+
+function configure_yuzu() {
+    mkRomDir "switch"
+    addEmulator 0 "$md_id" "switch" "$md_inst/bin/yuzu.AppImage -f -g %ROM%"
+    addSystem "switch"
+}
